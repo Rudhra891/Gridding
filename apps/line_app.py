81f823d33d2e4174274eef35cdd3d8b5ded37904
Rudhra891 <118509052+Rudhra891@users.noreply.github.com>
2025-10-17 16:00:32 +0530

    Update line_app.py

--- apps/line_app.py
+++ apps/line_app.py
@@ -17,35 +17,62 @@ def get_direction(row, center):
     return "C"
 
 def run():
-    st.title("📈 Geophysical Data – Traverses (Line View)")
+    #st.title("📈 Geophysical Data – Traverses (Line View)")
+    st.markdown("<h1 style='text-align: center;'>📈 Geophysical Data – Traverses<br><span style='font-size: 0.6em;'>(Line View)</span></h1>", unsafe_allow_html=True)
 
     # File upload
-    uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+    uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"],help="upload your data file, if need input template format: download below")
+    # Path to template excel file
+    template_path = "template_Rudra Geophysicist.xlsx"
+
+     # Download button
+    with open(template_path, "rb") as file:
+        st.download_button(
+            label="📥 Download Input Template",
+            data=file,
+            help ="Input template",
+            file_name="template_Rudra Geophysicist.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+    
     if not uploaded_file:
-        st.info("👆 Please upload an Excel file to proceed.")
+        #st.info("👆 Please upload an Excel file to proceed.")
         return
     df = pd.read_excel(uploaded_file)
-
+    
+    
+    
     # X-axis selection
-    axis_choice = st.radio("Plot Resistivity (Res) vs:", ("Longitude", "Latitude", "Ref"))
+    axis_choice = st.radio("Plot Resistivity (Res) vs:", ("Longitude", "Latitude", "Ref"),help="Click the parameter to see resistivity variations along the line respective to that parameter")
 
-    selected_line = st.selectbox("Select LineNumber", sorted(df["LineNumber"].unique()))
+    selected_line = st.selectbox("Select LineNumber", sorted(df["LineNumber"].unique()),help="select survey line number from the dropdown")
     filtered = df[df["LineNumber"] == selected_line]
 
     # Selected Refs with priorities
-    selected_refs = st.multiselect("Highlight Ref(s)", sorted(filtered["Ref"].unique()))
-    prio_text = st.text_input("Priority labels (comma separated to match Refs):", "")
+    selected_refs = st.multiselect("Highlight Data Points", sorted(filtered["Ref"].unique()),help="Select the referece(s) to highlight the priority point(s) on the graph")
+    prio_text = st.text_input("Priority labels (comma separated to match Refs):", "",help="Give the priority point(s) names **e.g P1,P2** as per '**Highlight Data Points**' order")
     priorities = [p.strip() for p in prio_text.split(",")] if prio_text else []
 
     # Ref range bars with colors and labels
-    ranges_input = st.text_input("Enter Ref ranges (e.g. 5:10,15:20):", "")
+     
+        
+    ref_range_help="""
+                Enter reference value range(small to big) to 
+                denote the geologica features/type of geology
+                with color bar. (e.g. 5:10,15:20):
+               """
+    ranges_input = st.text_input("Reference ranges ", "",help=ref_range_help)
     ref_ranges = [r.strip() for r in ranges_input.split(",") if ":" in r]
 
     bar_colors, bar_labels = [], []
     for rr in ref_ranges:
-        col = st.color_picker(f"Color for {rr}", "#DDDDDD")
+        col = st.color_picker(f"Color for {rr}", "#DDDDDD",help="pick the color for bar")
         bar_colors.append(col)
-        lbl = st.text_input(f"Legend label for {rr}", f"Range {rr}")
+        help_range_text = """
+                         write the geology/geological features/anything scientific
+                         related to this particular range (e.g. **weathered rock/ Fractured granite**)
+                         """
+        lbl = st.text_input(f"Legend label for {rr}", f"Range {rr}",help= help_range_text )
         bar_labels.append(lbl)
 
     # Compute bounding center for direction calculation
